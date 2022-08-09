d41126edf48b7cfc96958cdf8b88ca4aeb24b793
Alan Xu <alan.xu1699@gmail.com>
2022-08-08 22:13:50 -0400

    commit tree fd01b61bc1eba4fc199580fd97e44d50398e5597

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: [Fiction, Non-Fiction]
+
+    validate must_have_click_baity_title
+
+    def must_have_click_baity_title
+        if title === "Won't Believe" && "Secret" && "Top [number]" && "Guess"
+            error.add(:title, "Sorry your title is not clickbaity enough")
+        end
+    end
 end
